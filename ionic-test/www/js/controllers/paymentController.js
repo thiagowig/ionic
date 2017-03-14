6b48aab2ecc0257793dba6a7b97e1abe796dd960
Thiago Santos Fonseca <tfonseca@ciandt.com>
2017-03-13 21:01:53 -0300

    Infinite scroll

--- ionic-test/www/js/controllers/paymentController.js
+++ ionic-test/www/js/controllers/paymentController.js
@@ -1,4 +1,17 @@
 angular.module('starter.controllers')
 
   .controller('PaymentController', function ($scope, $stateParams) {
+
+    $scope.noMoreItemsAvailable = false;
+
+    $scope.loadMore = function () {
+      $scope.items.push({ id: $scope.items.length });
+
+      if ($scope.items.length == 99) {
+        $scope.noMoreItemsAvailable = true;
+      }
+      $scope.$broadcast('scroll.infiniteScrollComplete');
+    };
+
+    $scope.items = [];
   })
