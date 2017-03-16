9f9f5160726e5a5cfd4b6a7af6e39d9fcde1a754
Thiago Santos Fonseca <dev.thiago@gmail.com>
2017-03-16 20:20:20 -0300

    Changing configs

--- gac/www/js/services/common/dateService.js
+++ gac/www/js/services/common/dateService.js
@@ -2,18 +2,18 @@ angular.module('starter.services')
 
   .service('DateService', function ($rootScope, $ionicPopup, ConfigService) {
     this.calculateExpectedPaymentDate = function (idPaymentMethod, installment) {
+      var paymentDayOfWeek = parseInt($rootScope.configuration.paymentDayOfWeek)
+      var paymentDate = new Date()
 
       if (!installment) {
-        var paymentDayOfWeek = parseInt($rootScope.configuration.paymentDayOfWeek)
-        var paymentDate = new Date()
         paymentDate.setDate(paymentDate.getDate() + 7);
         paymentDate.setDate(paymentDate.getDate() + (paymentDayOfWeek + (7 - paymentDate.getDay())) % 7)
 
         return paymentDate.getTime()
       } else {
-        var today = new Date()
+        paymentDate.setMonth(paymentDate.getMonth() + installment)
 
-        return today.setMonth(today.getMonth() + installment)
+        return paymentDate.setDate(paymentDate.getDate() + (paymentDayOfWeek + (7 - paymentDate.getDay())) % 7)
       }
     }
   })
