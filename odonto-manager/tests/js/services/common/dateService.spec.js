3a9c539c576dee61372472721f6d29397e4860ee
Thiago Santos Fonseca <dev.thiago@gmail.com>
2017-03-23 19:41:08 -0300

    Adding some tests

--- odonto-manager/tests/js/services/common/dateService.spec.js
+++ odonto-manager/tests/js/services/common/dateService.spec.js
@@ -16,7 +16,7 @@ describe('dateService.spec.js', function () {
         });
     });
 
-    it('must return return 22/03/2017', inject(function (DateService) {
+    it('must return 22/03/2017', inject(function (DateService) {
         var dayRange = [20, 21]
 
         dayRange.forEach(function (day) {
@@ -36,7 +36,7 @@ describe('dateService.spec.js', function () {
         })
     }));
 
-    it('must return return 29/03/2017', inject(function (DateService) {
+    it('must return 29/03/2017', inject(function (DateService) {
         var dayRange = [22, 23, 24, 25, 26, 27, 28]
 
         dayRange.forEach(function (day) {
@@ -56,7 +56,7 @@ describe('dateService.spec.js', function () {
         })
     }));
 
-    it('must return return 05/04/2017', inject(function (DateService) {
+    it('must return 05/04/2017', inject(function (DateService) {
         var dayRange = [29, 30]
 
         dayRange.forEach(function (day) {
@@ -76,8 +76,8 @@ describe('dateService.spec.js', function () {
         })
     }));
 
-    it('must return return 05/04/2017', inject(function (DateService) {
-        var dayRange = [20, 21, 22, 23, 24, 25]
+    it('must return 26/04/2017 with installment', inject(function (DateService) {
+        var dayRange = [20, 21, 22, 23, 24, 25, 26]
 
         dayRange.forEach(function (day) {
             var currentDate = {
@@ -93,6 +93,106 @@ describe('dateService.spec.js', function () {
             }
 
             assertDate(DateService, currentDate, expectedDate, 1)
+        })
+    }));
+
+    it('must return 03/05/2017 with installment', inject(function (DateService) {
+        var dayRange = [27, 28, 29, 30]
+
+        dayRange.forEach(function (day) {
+            var currentDate = {
+                year: 2017,
+                month: 2,
+                day: day
+            }
+
+            var expectedDate = {
+                year: 2017,
+                month: 4,
+                day: 3
+            }
+
+            assertDate(DateService, currentDate, expectedDate, 1)
+        })
+    }));
+
+    it('must return 05/04/2017 with installment', inject(function (DateService) {
+        var dayRange = [27, 28, 29, 30]
+
+        dayRange.forEach(function (day) {
+            var currentDate = {
+                year: 2017,
+                month: 2,
+                day: day
+            }
+
+            var expectedDate = {
+                year: 2017,
+                month: 4,
+                day: 3
+            }
+
+            assertDate(DateService, currentDate, expectedDate, 1)
+        })
+    }));
+
+    it('must return 24/05/2017 with installment', inject(function (DateService) {
+        var dayRange = [18, 19, 20, 21, 22, 23, 24]
+
+        dayRange.forEach(function (day) {
+            var currentDate = {
+                year: 2017,
+                month: 2,
+                day: day
+            }
+
+            var expectedDate = {
+                year: 2017,
+                month: 4,
+                day: 24
+            }
+
+            assertDate(DateService, currentDate, expectedDate, 2)
+        })
+    }));
+
+    it('must return 31/05/2017 with installment', inject(function (DateService) {
+        var dayRange = [25, 26, 27, 28]
+
+        dayRange.forEach(function (day) {
+            var currentDate = {
+                year: 2017,
+                month: 2,
+                day: day
+            }
+
+            var expectedDate = {
+                year: 2017,
+                month: 4,
+                day: 31
+            }
+
+            assertDate(DateService, currentDate, expectedDate, 2)
+        })
+    }));
+
+    it('must return 06/12/2017 with installment', inject(function (DateService) {
+        var dayRange = [1, 2, 3, 4, 5, 6]
+
+        dayRange.forEach(function (day) {
+            var currentDate = {
+                year: 2017,
+                month: 2,
+                day: day
+            }
+
+            var expectedDate = {
+                year: 2017,
+                month: 11,
+                day: 6
+            }
+
+            assertDate(DateService, currentDate, expectedDate, 9)
         })
     }));
 
