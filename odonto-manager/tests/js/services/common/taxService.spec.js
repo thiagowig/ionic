d23156f57d88013b5eced28e5b39951c20f2f2b7
Thiago Santos Fonseca <dev.thiago@gmail.com>
2017-03-24 18:46:32 -0300

    Adding some rates test

--- odonto-manager/tests/js/services/common/taxService.spec.js
+++ odonto-manager/tests/js/services/common/taxService.spec.js
@@ -5,7 +5,7 @@ describe('taxService.spec.js', function () {
 
   beforeEach(module('starter.services'));
 
-  it('must calculate rates', inject(function (TaxService) {
+  it('must calculate rates for money payment', inject(function (TaxService) {
 
     var useCases = [
       {
@@ -22,10 +22,85 @@ describe('taxService.spec.js', function () {
           clinicValue: 550,
           receiveValue: 450,
         }
+      },
+      {
+        attendance: {
+          fullValue: 437
+        },
+        paymentMethodConfig: {
+          machineTax: 0,
+          clinicTax: 55
+        },
+        expected: {
+          fullValue: 437,
+          machineTaxValue: 0,
+          clinicValue: 240.35,
+          receiveValue: 196.65,
+        }
+      },
+      {
+        attendance: {
+          fullValue: 123.45
+        },
+        paymentMethodConfig: {
+          machineTax: 0,
+          clinicTax: 55
+        },
+        expected: {
+          fullValue: 123.45,
+          machineTaxValue: 0,
+          clinicValue: 67.90,
+          receiveValue: 55.55,
+        }
       }
     ]
 
-    useCases.forEach(function(useCase) {
+    useCases.forEach(function (useCase) {
+      TaxService.calculateRates(useCase.attendance, useCase.paymentMethodConfig)
+
+      expect(useCase.attendance.fullValue).toBe(useCase.expected.fullValue)
+      expect(useCase.attendance.machineTaxValue).toBe(useCase.expected.machineTaxValue)
+      expect(useCase.attendance.clinicValue).toBe(useCase.expected.clinicValue)
+      expect(useCase.attendance.receiveValue).toBe(useCase.expected.receiveValue)
+    })
+  }));
+
+  it('must calculate rates for debt payment', inject(function (TaxService) {
+
+    var useCases = [
+      {
+        attendance: {
+          fullValue: 1000
+        },
+        paymentMethodConfig: {
+          machineTax: 2,
+          clinicTax: 55
+        },
+        expected: {
+          fullValue: 1000,
+          machineTaxValue: 20,
+          clinicValue: 539,
+          receiveValue: 441,
+        }
+      },
+      {
+        attendance: {
+          fullValue: 437
+        },
+        paymentMethodConfig: {
+          machineTax: 2,
+          clinicTax: 55
+        },
+        expected: {
+          fullValue: 437,
+          machineTaxValue: 0,
+          clinicValue: 240.35,
+          receiveValue: 196.65,
+        }
+      },
+    ]
+
+    useCases.forEach(function (useCase) {
       TaxService.calculateRates(useCase.attendance, useCase.paymentMethodConfig)
 
       expect(useCase.attendance.fullValue).toBe(useCase.expected.fullValue)
