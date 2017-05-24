84dc0c7b3b04daeb14b180627b7cf28d6ab63a23
Thiago Fonseca <dev.thiago@gmail.com>
2017-05-24 17:54:13 -0300

    Removing unused code

--- odonto-manager/www/js/controllers/helpController.js
+++ odonto-manager/www/js/controllers/helpController.js
@@ -1,12 +1,5 @@
 angular.module('starter.controllers')
 
   .controller('HelpController', function ($cordovaFile, PopupService) {
-    $cordovaFile.writeFile(cordova.file.externalDataDirectory, 'myTestFile.txt', 'Thiago Fonseca is REALLY awesome', true)
-      .then(function (result) {
-        PopupService.success('Sucesso ao salvar arquivo')
-        console.log(JSON.stringify(result))
-      }, function (err) {
-        PopupService.erro('Erro ao salvar arquivo')
-        console.log(JSON.stringify(err))
-      })
+    
   })
